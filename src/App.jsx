ab38ffca6095427ba0de34b7bea1f10ea5319ef0
Aliraza324 <alirazavn2342@gmail.com>
2026-06-09 17:21:38 +0500

    feat: integrate ToastProvider in App component and add new video assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useIsFetching, useIsMutating } from '@tanstack/react-query'
 import AppRoutes from './routes/AppRoutes.jsx'
 import GlobalLoader from './components/shared/GlobalLoader.jsx'
+import { ToastProvider } from './utils/ToastProvider.jsx'
 
 const App = () => {
   const isFetching = useIsFetching()
@@ -8,10 +9,10 @@ const App = () => {
   const isBusy = isFetching + isMutating > 0
 
   return (
-    <>
+    <ToastProvider>
       <AppRoutes />
       <GlobalLoader active={isBusy} overlay message="Please wait..." />
-    </>
+    </ToastProvider>
   )
 }
 
